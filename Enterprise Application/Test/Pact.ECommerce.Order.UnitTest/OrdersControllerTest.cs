f48075e4ae128ddec162880864d716bdd106e24d
skunani <suneel.kunani@hotmail.com>
2020-11-28 14:32:28 +0530

    Unit tests with .net5

--- Enterprise Application/Test/Pact.ECommerce.Order.UnitTest/OrdersControllerTest.cs
+++ Enterprise Application/Test/Pact.ECommerce.Order.UnitTest/OrdersControllerTest.cs
@@ -1,15 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.QualityTools.Testing.Fakes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using Packt.Ecommerce.DTO.Models;
 using Packt.Ecommerce.Order.Contracts;
-using Packt.Ecommerce.Order.Contracts.Fakes;
 using Packt.Ecommerce.Order.Controllers;
-using Packt.Ecommerce.Order.Services.Fakes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Pact.ECommerce.Order.UnitTest
@@ -17,7 +11,6 @@ namespace Pact.ECommerce.Order.UnitTest
     [TestClass]
     public class OrdersControllerTest
     {
-
         [TestMethod]
         public async Task OrderController_Constructor()
         {
@@ -25,30 +18,25 @@ namespace Pact.ECommerce.Order.UnitTest
             Assert.IsNotNull(testObject);
         }
 
-
         [TestMethod]
         public async Task When_GetOrdersAsync_with_ExistingOrder_receive_OkObjectResult()
         {
-            var stub = new StubIOrderService()
-            {
-                GetOrderByIdAsyncString = (x) => Task.FromResult<OrderDetailsViewModel>(new OrderDetailsViewModel() { Id = "1" }),
-            };
+            var stub = new Mock<IOrderService>();
+            stub.Setup(x => x.GetOrderByIdAsync(It.IsAny<string>())).Returns(Task.FromResult<OrderDetailsViewModel>(new OrderDetailsViewModel { Id = "1" }));
+            OrdersController testObject = new OrdersController(stub.Object);
 
-            OrdersController testObject = new OrdersController(stub);
-            var order = (await testObject.GetOrderById("1"));
+            var order = await testObject.GetOrderById("1").ConfigureAwait(false);
             Assert.IsInstanceOfType(order, typeof(OkObjectResult));
         }
 
         [TestMethod]
         public async Task When_GetOrdersAsync_with_No_ExistingOrder_receive_NotFoundResult()
         {
-            var stub = new StubIOrderService()
-            {
-                GetOrderByIdAsyncString = (x) => Task.FromResult<OrderDetailsViewModel>(null),
-            };
+            var stub = new Mock<IOrderService>();
+            stub.Setup(x => x.GetOrderByIdAsync(It.IsAny<string>())).Returns(Task.FromResult<OrderDetailsViewModel>(null));
+            OrdersController testObject = new OrdersController(stub.Object);
 
-            OrdersController testObject = new OrdersController(stub);
-            var order = (await testObject.GetOrderById("1"));
+            var order = await testObject.GetOrderById("1").ConfigureAwait(false);
             Assert.IsInstanceOfType(order, typeof(NotFoundResult));
         }
     }
