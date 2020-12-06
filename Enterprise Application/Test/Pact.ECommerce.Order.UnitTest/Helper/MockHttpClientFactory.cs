8fa8860a261b1a3b359937a23ac9cf42fc90b74b
skunani <suneel.kunani@hotmail.com>
2020-12-06 08:24:11 +0530

    Added Order Service Test

---
+++ Enterprise Application/Test/Pact.ECommerce.Order.UnitTest/Helper/MockHttpClientFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pact.ECommerce.Order.UnitTest.Helper
+{
+    public class MockHttpClientFactory : IHttpClientFactory
+    {
+        public HttpResponseMessage ResponseMessage = new HttpResponseMessage(HttpStatusCode.OK);
+        public IList<MockHttpMessageHandler> MessageHandlers = new List<MockHttpMessageHandler>();
+
+        public HttpClient CreateClient(string name = default)
+        {
+            var messageHandler = new MockHttpMessageHandler(this.ResponseMessage);
+            this.MessageHandlers.Add(messageHandler);
+            return new HttpClient(messageHandler);
+        }
+    }
+
+    public class MockHttpMessageHandler : HttpMessageHandler
+    {
+        private readonly HttpResponseMessage responseMessage;
+        public HttpRequestMessage Request;
+
+        public MockHttpMessageHandler(HttpResponseMessage response)
+        {
+            this.responseMessage = response;
+        }
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            this.Request = request;
+            return await Task.FromResult(responseMessage);
+        }
+    }
+}
---
+++ Enterprise Application/Test/Pact.ECommerce.Order.UnitTest/OrderServiceTest.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Newtonsoft.Json;
+using Packt.Ecommerce.Caching.Interfaces;
+using Packt.Ecommerce.Common.Options;
+using Packt.Ecommerce.Order;
+using Packt.Ecommerce.Order.Services;
+using Pact.ECommerce.Order.UnitTest.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pact.ECommerce.Order.UnitTest
+{
+    [TestClass]
+    public class OrderServiceTest
+    {
+        [TestMethod]
+        public async Task When_GetOrderByIdAsync_with_ExistingOrder_receive_Order()
+        {
+            Packt.Ecommerce.Data.Models.Order orderResponse = new Packt.Ecommerce.Data.Models.Order { Id = "1" };
+            var httpClientFactory = new MockHttpClientFactory()
+            {
+                ResponseMessage = new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(JsonConvert.SerializeObject(orderResponse))
+                }
+            };
+
+            IOptions<ApplicationSettings> mockOptions = Options.Create<ApplicationSettings>(new ApplicationSettings { DataStoreEndpoint = "https://orderStore.pact.com" });
+            Mock<IDistributedCacheService> mockCacheService = new Mock<IDistributedCacheService>();
+
+            var mockMapper = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new AutoMapperProfile()); 
+            });
+            var mapper = mockMapper.CreateMapper();
+
+
+            OrdersService testObject = new OrdersService(httpClientFactory, mockOptions, mapper,  mockCacheService.Object);
+            var order = testObject.GetOrderByIdAsync("1");
+
+            Assert.AreEqual("1", order.Id);
+        }
+    }
+}
